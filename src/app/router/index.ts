cc24afa27e3875a7b8941f1f75abea8f95c188d8
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2023-12-18 16:29:25 +0300

    refactor: added FormsPage

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomePage from '../../pages/HomePage.vue';
+import HomePage from '@/pages/HomePage.vue';
+import FormsPage from '@/pages/FormsPage.vue';
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
-	routes: [
-		{
-			path: '/',
-			name: 'home',
-			component: HomePage,
-		},
-	],
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomePage
+    },
+    {
+      path: '/forms',
+      name: 'forms',
+      component: FormsPage
+    }
+  ]
 });
 
 export default router;
